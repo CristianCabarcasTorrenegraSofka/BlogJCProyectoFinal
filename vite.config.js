1a9f1fdb014f7c7899939b8b233854c4a0c5e241
CristianCabarcasTorrenegraSofka <cristian.cabarcas@sofka.com.co>
2024-11-12 22:44:37 -0500

    merge

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/BlogJCProyectoFinal/",
+  base: "https://cristiancabarcastorrenegrasofka.github.io/BlogJCProyectoFinal",
   plugins: [react()],
 })
